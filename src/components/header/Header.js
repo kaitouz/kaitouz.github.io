e5bd5f7908f6a8323796cd5724d45b9e30a3e9c9
kaitouz <thienkaito@gmail.com>
2022-11-15 05:19:50 +0300

    try to add mobile view

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,33 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./header.css";
 import { Link , useLocation } from "react-router-dom";
+import { VscGrabber, VscClose } from "react-icons/vsc";
+
 
 export const Header = () => {
 
     const location = useLocation()
 
+    const [isToggleActive, setToggleActive] = useState("true");
+    const handleToggle = () => {
+        setToggleActive(!isToggleActive);
+    };
+
     return (
-        <>
-            <header className="site__header fixed-top">
-                <div className="d-flex align-items-center justify-content-between pt-3">
+        <header className={isToggleActive ? 'header_normal fixed-top' : 'header_show fixed-top'}>
+            <div className="d-flex align-items-top justify-content-between pt-3">
 
-                    <Link  className="navbar-brand nav_ac" to="/">
-                    Kaitouz
-                    </Link>
-                    
-                    <div class="header__links">
-                    {/* <p>{window.location.pathname}</p> */}
-                    <Link className={location.pathname === '/' ? 'header__link_active' : 'header__link'} to="/">Home</Link>
-                    <Link className={location.pathname === '/about' ? 'header__link_active' : 'header__link'} to="/about">About</Link>
-                    <Link className={location.pathname === '/portfolio' ? 'header__link_active' : 'header__link'} to="/portfolio">Portfolio</Link>
-                    <Link className={location.pathname === '/contact' ? 'header__link_active' : 'header__link'} to="/contact">Contact</Link>
-                    </div>
-
-                    <div className="d-flex align-items-center">
-                    
-                    </div>
+                <Link  className="navbar-brand nav_ac" to="/">
+                Kaitouz
+                </Link>
+                
+                <div class={isToggleActive ? 'header__links' : 'header__links'}>
+                {/* <p>{window.location.pathname}</p> */}
+                <Link  className={location.pathname === '/' ? 'header__link_active' : 'header__link'} to="/">Home</Link>
+                <Link  className={location.pathname === '/about' ? 'header__link_active' : 'header__link'} to="/about">About</Link>
+                <Link  className={location.pathname === '/portfolio' ? 'header__link_active' : 'header__link'} to="/portfolio">Portfolio</Link>
+                <Link  className={location.pathname === '/contact' ? 'header__link_active' : 'header__link'} to="/contact">Contact</Link>
                 </div>
-            </header>
-        </>
+
+                <button className="header_content_toggle nav_ac" onClick={handleToggle}>
+                    {!isToggleActive ? <VscClose /> : <VscGrabber />}
+                </button>
+            </div>
+        </header>
     )
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,44 +8,47 @@ export const Home = () => {
     <section id="home" className="home">
         
         <div className="intro_sec d-block d-lg-flex align-items-center ">
-            <div className="hero-image order-lg-1"
+            <div className="hero-image"
                 style={{ backgroundImage: `url(${'/hero.png'})` }}>
             </div>
 
-            <div className="content order-lg-2 justify-content-center align-self-center">
-              <div className="intro">
-                <div>
-                  <h2>I'm Thien Le</h2>
-                  <h1>
-                    <Typewriter
-                      options={{
-                        strings: [
-                          "I'm currently studying at ITMO University",
-                          "My specialization is Software Engineering",
-                          "I'm digging into Deep Learning and Big Data",
-                        ],
-                        autoStart: true,
-                        loop: true,
-                        deleteSpeed: 10,
-                      }}
-                    />
-                  </h1>
-                </div>
-                <p>Welcome to Kaitouz's homepage</p>
+            <div className="content justify-content-center align-self-center">
+              <div className='align-self-center'>
 
-                <div className="intro_btn-action pb-5">
-                  <Link to="/about">
-                    <div id="button_white" className="ac_btn btn ">
-                      About me
-                    </div>
-                  </Link>
-                  <Link to="/contact">
-                    <div id="button_black" className="ac_btn btn">
-                      Contact Me
-                    </div>
-                  </Link>
-                  
-                </div>
+              
+                      <div className="intro">
+                        <div>
+                          <h2>I'm Thien Le</h2>
+                          <h1>
+                            <Typewriter
+                              options={{
+                                strings: [
+                                  "I'm currently studying at ITMO University",
+                                  "My specialization is Software Engineering",
+                                  "I'm digging into Deep Learning and Big Data",
+                                ],
+                                autoStart: true,
+                                loop: true,
+                                deleteSpeed: 10,
+                              }}
+                            />
+                          </h1>
+                        </div>
+                        <p>Welcome to Kaitouz's homepage</p>
+
+                        <div className="intro_btn-action pb-5">
+                          <Link to="/about">
+                            <div id="button_white" className="ac_btn btn ">
+                              About me
+                            </div>
+                          </Link>
+                          <Link to="/contact">
+                            <div id="button_black" className="ac_btn btn">
+                              Contact Me
+                            </div>
+                          </Link>
+                          </div>
+                        </div>
               </div>
           </div>
 
